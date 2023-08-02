9ad210859959e03134aecdf4a0df35844d8857b7
Sergio del Amo <sergio.delamo@softamo.com>
2023-08-02 11:49:57 +0200

    annotate test with @PendingFeature

--- multitenancy/src/test/groovy/io/micronaut/multitenancy/tenantresolver/HttpRequestTenantResolverValidationSpec.groovy
+++ multitenancy/src/test/groovy/io/micronaut/multitenancy/tenantresolver/HttpRequestTenantResolverValidationSpec.groovy
@@ -5,6 +5,7 @@ import io.micronaut.core.util.StringUtils
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import jakarta.validation.ConstraintViolationException
+import spock.lang.PendingFeature
 import spock.lang.Specification
 
 @Property(name = "micronaut.multitenancy.tenantresolver.httpheader.enabled", value = StringUtils.TRUE)
@@ -13,6 +14,7 @@ class HttpRequestTenantResolverValidationSpec extends Specification {
     @Inject
     HttpRequestTenantResolver tenantResolver
 
+    @PendingFeature
     void "HttpRequestTenantResolver::resolveTenantIdentifier(HttpRequest) validates request is not null"() {
         when:
         tenantResolver.resolveTenantIdentifier(null)
