1d367143891cd293e10dded10b6a35f797246e82
Sergio del Amo <sergio.delamo@softamo.com>
2023-09-11 16:22:27 +0200

    test: add SubdomainTenantResolver tests

--- multitenancy/src/test/groovy/io/micronaut/multitenancy/tenantresolver/SubdomainTenantResolverEnabledSpec.groovy
+++ multitenancy/src/test/groovy/io/micronaut/multitenancy/tenantresolver/SubdomainTenantResolverEnabledSpec.groovy
@@ -17,7 +17,11 @@ package io.micronaut.multitenancy.tenantresolver
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
+import io.micronaut.http.HttpHeaders
+import io.micronaut.http.HttpRequest
+import io.micronaut.multitenancy.exceptions.TenantNotFoundException
 import spock.lang.AutoCleanup
+import spock.lang.PendingFeature
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -40,5 +44,72 @@ class SubdomainTenantResolverEnabledSpec extends Specification {
         and:
         context.containsBean(HttpRequestTenantResolver)
         context.getBean(HttpRequestTenantResolver) instanceof SubdomainTenantResolver
+    }
+
+    @PendingFeature
+    void "TenantResolver resolves tenant identifier without host header"() {
+        given:
+        HttpRequest<?> request = HttpRequest.GET(uri)
+
+        expect:
+        context.getBean(SubdomainTenantResolver).resolveTenantIdentifier(request) == expected
+
+        and:
+        new SubdomainTenantResolver().resolveTenantIdentifier(request) == expected
+
+        where:
+        uri                                              | expected
+        'https://example.com/path/resource'              | 'DEFAULT'
+        'https://www.example.com/path/resource'          | 'www'
+        'https://domain.co.uk/path/resource'             | 'DEFAULT'
+        'https://duper.domain.co.uk/path/resource'       | 'duper'
+        'https://super.duper.domain.co.uk/path/resource' | 'super.duper'
+        'https://example.com/path/resource'              | 'DEFAULT'
+    }
+
+    void "TenantResolver resolves tenant identifier"(String uri, String header, String expected) {
+        given:
+        HttpRequest<?> request = HttpRequest.GET(uri).header(HttpHeaders.HOST, header)
+
+        expect:
+        context.getBean(SubdomainTenantResolver).resolveTenantIdentifier(request) == expected
+
+        and:
+        new SubdomainTenantResolver().resolveTenantIdentifier(request) == expected
+
+        where:
+        uri                                              | header                     | expected
+        'https://www.example.com/path/resource'          | ''                         | 'DEFAULT'
+        'https://www.example.com/path/resource'          | 'test'                     | 'DEFAULT'
+        'https://example.com/path/resource'              | 'www.test.com'             | 'www'
+        'https://example.com/path/resource'              | 'duper.domain.co.uk'       | 'duper'
+        'https://example.com/path/resource'              | 'lorem.ipsum.dev'          | 'lorem'
+    }
+
+    @PendingFeature
+    void "TenantResolver resolves tenant identifier with header"(String uri, String header, String expected) {
+        given:
+        HttpRequest<?> request = HttpRequest.GET(uri).header(HttpHeaders.HOST, header)
+
+        expect:
+        context.getBean(SubdomainTenantResolver).resolveTenantIdentifier(request) == expected
+
+        and:
+        new SubdomainTenantResolver().resolveTenantIdentifier(request) == expected
+
+        where:
+        uri                                              | header                     | expected
+        'https://www.example.com/path/resource'          | 'test.com'                 | 'DEFAULT'
+        'https://www.example.com/path/resource'          | 'test.com'                 | 'DEFAULT'
+        'https://example.com/path/resource'              | 'domain.co.uk'             | 'DEFAULT'
+        'https://example.com/path/resource'              | 'super.duper.domain.co.uk' | 'super.duper'
+    }
+
+    void "TenantResolver resolves tenant identifier - no http request"() {
+        when:
+        new SubdomainTenantResolver().resolveTenantIdentifier()
+
+        then:
+        thrown(TenantNotFoundException)
     }
 }
