2eb9daf98a20eea10065f8b5dbf5535fc5b63318
Tim Yates <tim.yates@gmail.com>
2023-11-21 15:48:36 +0000

    Appease sonar's nonsense

--- test-suite/src/test/java/io/micronaut/multitenancy/Tests.java
+++ test-suite/src/test/java/io/micronaut/multitenancy/Tests.java
@@ -3,6 +3,7 @@ package io.micronaut.multitenancy;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MutableHttpRequest;
+import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
@@ -22,13 +23,15 @@ class Tests {
 
     @Test
     void testSecuredAnnotationCanSeeTenantIds() {
+        BlockingHttpClient blockingClient = client.toBlocking();
+
         // Sergio requires authentication as the tenant is not 'allowed'
         MutableHttpRequest<Object> sergio = HttpRequest.GET("/").header("X-Tenant", "sergio");
-        HttpClientResponseException thrown = assertThrows(HttpClientResponseException.class, () -> client.toBlocking().exchange(sergio));
+        HttpClientResponseException thrown = assertThrows(HttpClientResponseException.class, () -> blockingClient.exchange(sergio));
         assertEquals(HttpStatus.UNAUTHORIZED, thrown.getStatus());
 
         // But the allowed request has tenant as allowed so secured annotation evaluates to true and the request is allowed
         MutableHttpRequest<Object> tim = HttpRequest.GET("/").header("X-Tenant", "allowed");
-        assertEquals("Hello World", client.toBlocking().retrieve(tim));
+        assertEquals("Hello World", blockingClient.retrieve(tim));
     }
 }
